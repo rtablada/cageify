d0354a9bb407e2704ce2d817ae4cc6e55d5c43db
Ryan Tablada <ryan.tablada@gmail.com>
2015-05-22 09:36:32 -0500

    Cagify!!!

---
+++ js/cagify.js
@@ -0,0 +1,16 @@
+(function () {
+	$.fn.cagify = function() {
+		var imgs = this.find('img');
+
+		imgs.each(function (i, el) {
+			debugger;
+			var $el = $(el),
+				height = $el.height(),
+				width = $el.width(),
+				url = 'http://www.placecage.com/' + width + '/' + height;
+
+
+			$el.attr('src', url);
+		});
+	}
+})();
